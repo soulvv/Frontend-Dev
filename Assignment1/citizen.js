cfd8f266cc80693c5b349ab86e63d25f2b7ee3b7
Ayush Vishwakarma <152463781+soulvv@users.noreply.github.com>
2025-11-19 11:23:07 +0530

    Update citizen.js

---
+++ Assignment1/citizen.js
@@ -0,0 +1,20 @@
+// q1.js - Personalized Login Greeting
+
+// Change this to test with different users
+const userName = "Batsy";
+
+// Get current hour (0–23) from system clock
+const currentHour = new Date().getHours();
+
+let greeting;
+
+// Decide greeting based on time of day
+if (currentHour < 12) {
+  greeting = `Good Morning ${userName}!`;
+} else if (currentHour >= 12 && currentHour < 17) {
+  greeting = `Good Afternoon ${userName}!`;
+} else {
+  greeting = `Good Evening ${userName}!`;
+}
+
+console.log(greeting);
